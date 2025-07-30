448b4dffbf0209913b7550cbb27c2c0109fb97e5
tarrow9 <gkgkrnjs123@naver.com>
2025-07-30 11:31:28 +0900

    fix generator - add file writer

--- BDAlchTimerGen.py
+++ BDAlchTimerGen.py
@@ -19,3 +19,13 @@ for i in range(1, 11):
         "seconds": seconds,
         "hotkey": "ctrl+alt+" + str(i)[-1]
     }
+
+sorted_timers = {}
+for group_key, timer_dict in sorted(timers.items()):
+    sorted_timers[group_key] = {
+        title: config for title, config in sorted(timer_dict.items())
+    }
+timers = sorted_timers
+
+with open(TIMER_FILE, "w", encoding="utf-8") as f:
+    json.dump(dict(timers), f, ensure_ascii=False, indent=2)
